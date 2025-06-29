6a2bcadb3cb475256a9d439c5b4d96656b6e7e86
Hayden <haydenhansondev+GitHub@gmail.com>
2025-06-28 20:23:17 -0400

    feat(lexer): add lexer struct and utility methods

---
+++ lexer/lexer.go
@@ -0,0 +1,65 @@
+package lexer
+
+import (
+	"fmt"
+	"interpreter/token"
+)
+
+type Lexer struct {
+	input        string // the file contents
+	position     int    // current position in input
+	readPosition int    // current reading position in input
+	ch           byte   // current char
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		fmt.Println(string(l.ch))
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	// fmt.Println(string(l.ch))
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	l.readChar()
+	return tok
+}
